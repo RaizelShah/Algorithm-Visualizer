27cda6f8ed7751b74539ff8bb79737311e84d662
RaizelShah <shahraizel11@gmail.com>
2024-09-27 22:35:14 +0530

    Update animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,45 @@ export default {
   theme: {
     extend: {
       keyframes: {
+        traversed: {
+          "0%": {
+            transform: "scale(0.3)",
+            backgroundColor: "#9333eabf",
+            borderRadius: "100%",
+          },
+          "50%": {
+            backgroundColor: "#4f46e5bf",
+          },
+          "75%": {
+            transform: "scale(1.2)",
+            backgroundColor: "#3b82f6bf",
+          },
+          "100%": {
+            transform: "scale(1)",
+            backgroundColor: "#22d3ee",
+          },
+        },
+        path: {
+          "0%": {
+            transform: "scale(0.3)",
+            backgroundColor: "#e11d48bf",
+            borderRadius: "100%",
+          },
+          "50%": {
+            backgroundColor: "#ea580cbf",
+          },
+          "75%": {
+            transform: "scale(1.2)",
+            backgroundColor: "#fb923cbf",
+          },
+          "90%": {
+            transform: "scale(0.8)",
+            backgroundColor: "#fde68a",
+          },
+          "100%": {
+            transform: "scale(1)",
+          },
+        },
         wall: {
           "0%": {
             transform: "scale(0.7)",
@@ -12,6 +51,11 @@ export default {
             transform: "scale(1)",
           },
         },
+      },
+      animation: {
+        traversed: "traversed 0.5s cubic-bezier(0, 0, 0.2, 1)",
+        path: "path 1.5s cubic-bezier(0, 0, 0.2, 1)",
+        wall: "wall 0.3s cubic-bezier(0.4, 0, 0.2, 1)",
       },
     },
   },
